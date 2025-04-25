be4d5402b5dbeb00ed2e7d5cad4404725ff68500
patel2019 <iapatel@uwm.edu>
2025-04-24 19:13:55 -0500

    Added models to admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import  Department, Course, Prerequisite, Semester, Section
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Course)
+admin.site.register(Prerequisite)
+admin.site.register(Semester)
+admin.site.register(Section)
